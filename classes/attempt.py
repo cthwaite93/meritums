b6fa04699471b5f77a74c920e2136df0ee8ecd3b
Cristofol Thwaite <cristofol.thwaite@copernic.cat>
2023-02-24 12:28:44 +0100

    Changed candidate with attempt logic

--- classes/attempt.py
+++ classes/attempt.py
@@ -1,15 +1,10 @@
 class Attempt:
-    def __init__(self, specialty_code, priority, points):
+    def __init__(self, specialty_code, specialty_name, section, priority, points):
         self.specialty_code = specialty_code
+        self.specialty_name = specialty_name
+        self.section = section
         self.priority = priority
         self.points = points
-        self.attempted = False
 
     def __lt__(self, other):
         return self.priority < other.priority
-
-    def hasBeenAttempted(self):
-        return self.attempted
-
-    def tried(self):
-        self.attempted = True
--- classes/candidate.py
+++ classes/candidate.py
@@ -1,20 +1,25 @@
+import bisect
+from attempt import Attempt
+
+
 class Candidate:
     def __init__(self, specialty_code, specialty_name, section, full_name, points, tribunal, priority):
-        self.specialty_code = specialty_code
-        self.specialty_name = specialty_name
-        self.section = section
-        self.full_name = full_name
-        self.points = points
-        self.tribunal = tribunal
-        self.priority = priority
         # This is done because there are some people who are different with the exact same full name, so we can identify
         # a candidate that might have different entries with different priorities. There can never be two candidates
         # with the same candidate_id and priority
-        self.candidate_id = self.full_name + self.tribunal
+        self.candidate_id = full_name + tribunal
+        self.full_name = full_name
+        self.tribunal = tribunal
+        self.attempts = [Attempt(specialty_code, specialty_name, section, points, priority)]
 
-    def __lt__(self, other):  # Sorting function that orders priority by ascent and points by descent
-        if self.priority < other.priority:
-            return True
-        elif self.priority == other.priority:
-            return self.points > other.points
-        return False
+    def __lt__(self, other):  # Sorting function that orders points by descent
+        return self.currentAttempt().points > other.currentAttempt().points
+
+    def addAttempt(self, specialty_code, specialty_name, section, points, priority):
+        bisect.insort_left(self.attempts, Attempt(specialty_code, specialty_name, section, points, priority))
+
+    def currentAttempt(self):
+        return self.attempts[0]
+
+    def removeCurrentAttempt(self):
+        self.attempts.pop(0)
